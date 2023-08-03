4170c94c291e9f50a9b82861adce318b35531b18
blackbrokkoli <kastomat@gmail.com>
2023-08-03 17:02:07 +0200

    clean up +page a bit, and add basic content for each step

---
+++ IDEAS.md
@@ -0,0 +1,1 @@
+If I let the user export flashcards at the end, that would be an ideal oppo to link to a generic SR tool I'm writing.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,47 +5,130 @@
 	import WriteTranslation from '../components/writeTranslation.svelte';
 </script>
 
-<main class="text-center container card p">
-    <div class="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-
-	<ul data-role="tabs" data-expand="true" id="tabs">
-		<li><a href="#_target_1">Initial Settings</a></li>
-		<li><a href="#_target_2">Image Prompt</a></li>
-		<li><a href="#_target_3">Translation Attempt</a></li>
-		<li><a href="#_target_4">Automatic Translation</a></li>
-		<li><a href="#_target_5">Write it</a></li>
-		<li><a href="#_target_6">Explanation</a></li>
-		<li><a href="#_target_7">Pronounciation</a></li>
-		<li><a href="#_target_8">Vocabulary</a></li>
-		<li><a href="#_target_9">Check</a></li>
-	</ul>
-	<div class="border bd-default no-border-top p-2">
-		<div id="_target_1">
-			<div class="">
-				<label for="native-language">Native Language</label>
-				<select name="native-language" id="native-language">
-					<option value="English">English</option>
-				</select>
+<main class="text-center container p">
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Initial Settings</h2>
+			<div id="_target_1">
+				<div class="">
+					<label for="native-language">Native Language</label>
+					<select name="native-language" id="native-language">
+						<option value="English">English</option>
+					</select>
+				</div>
+				<div class="">
+					<label for="target-language">Target Language</label>
+					<select name="target-language" id="target-language">
+						<option value="Japanese">Arabic</option>
+					</select>
+				</div>
 			</div>
-			<div class="">
-				<label for="target-language">Target Language</label>
-				<select name="target-language" id="target-language">
-					<option value="Japanese">Arabic</option>
-				</select>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
 			</div>
 		</div>
-		<div id="_target_2">The shield is a post-apocalyptic nanomachine.</div>
-		<div id="_target_3">Cream soup is just not the same without basil</div>
 	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Image Prompt</h2>
+			<p>Write something that comes to mind when seeing this image</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Translation Attempt</h2>
+			<p>Try to translate the sentence by yourself. Give it your best shot!</p>
+            <textarea name="" id="" cols="100" rows="10"></textarea>
+            <small>You don't have to put your translation here, you can also write it by hand</small>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Automatic Translation</h2>
+			<p>Check out the automatic translation of the sentence. I recommend the following steps:</p>
+            <ul>
+                <li>Compare with your own attempt.</li>
+                <li>Listen.</li>
+            </ul>
+            <p>
+                Afterwards, please copy the translated sentence here:
+            </p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Write it</h2>
+			<p>Copy the translated sentence (either per hand or in the input box below)</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Pronounciation</h2>
+			<p>Dive deeper into the pronounciation of each word by checking out the links below</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Explanation</h2>
+			<p>Copy the following prompt into ChatGPT. Take a minute to read it</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Vocabulary</h2>
+			<p>Do you still remember the words? You may write the answer in the input field (or not).</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">optional: Flashcards</h2>
+			<p>Copy the following prompt into ChatGPT to generate learning cards. These will be used to solidy the learned material for the rest of the session, and you can download the flashcards at the end.</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Next Step</button>
+			</div>
+		</div>
+	</div>
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Reflect</h2>
+			<p>Take a minute to reflect on what you learned in this run. Maybe there is something you want to note down?</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">New Prompt</button>
+			</div>
+		</div>
+	</div>
+
+
 	<!-- {#if currentStep === 1}
 		<NativeExpression />
 	{:else if currentStep === 2}
