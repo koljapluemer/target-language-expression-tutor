f2cb2fe1d5a0f54bfa45fcf150dfeb932eb5f6b3
blackbrokkoli <kastomat@gmail.com>
2023-08-09 20:43:09 +0200

    minor restructure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,10 +97,7 @@
 		// open chatgpt
 		window.open('https://chat.openai.com/', '_blank');
 	}
-
 </script>
-
-
 
 <main class="text-center p flex flex-col items-center">
 	<ul class="steps">
@@ -112,7 +109,7 @@
 		<li class={page >= 5 ? 'step step-primary' : 'step'}>Pronounciation</li>
 		<li class={page >= 6 ? 'step step-primary' : 'step'}>Explanation</li>
 		<li class={page >= 7 ? 'step step-primary' : 'step'}>Vocabulary</li>
-		<li class={page >= 8 ? 'step step-primary' : 'step'}>Grammar</li>
+		<li class={page >= 8 ? 'step step-primary' : 'step'}>Reflect</li>
 	</ul>
 
 	{#if page === 0}
@@ -313,12 +310,17 @@
 			<div class="card-body">
 				<h2 class="card-title">Explanation</h2>
 				<p>Copy the following prompt into ChatGPT. Take a minute to read it</p>
-				<textarea id="prompt" readonly class="bg-neutral-900/50 p-2 text-sm text-neutral-700 whitespace-normal" value='
+				<textarea
+					id="prompt"
+					readonly
+					class="bg-neutral-900/50 p-2 text-sm text-neutral-700 whitespace-normal"
+				>
 					You are an {targetLanguage.language} teacher, I am your pupil. I translated "{nativeExpression}"
 					to "{translatedExpression}". Explain me the sentence, the words in it and grammatical
 					structures. If there are special constructions, snares, common confusion or other
 					interesting things, please point them out. Please adjust the depth of your explanations to
-					the complexity of the sentence. Please explain in {nativeLanguage.language}.' />
+					the complexity of the sentence. Please explain in {nativeLanguage.language}."
+				</textarea>
 				<button class="btn btn-primary" on:click={copy}> Copy Prompt and Open ChatGPT </button>
 				<div class="card-actions justify-end">
 					<button
@@ -357,30 +359,16 @@
 	{#if page === 8}
 		<div class="card w-2/3 p4 mt-5 bg-base-100 shadow-xl">
 			<div class="card-body">
+				<h2 class="card-title">Reflect</h2>
+				<p>
+					Take a minute to reflect on what you learned in this run. Maybe there is something you
+					want to note down?
+				</p>
 				<h2 class="card-title">optional: Flashcards</h2>
 				<p>
 					Copy the following prompt into ChatGPT to generate learning cards. These will be used to
 					solidy the learned material for the rest of the session, and you can download the
 					flashcards at the end.
-				</p>
-				<div class="card-actions justify-end">
-					<button
-						on:click={() => {
-							page += 1;
-						}}
-						class="btn">Next Step</button
-					>
-				</div>
-			</div>
-		</div>
-	{/if}
-	{#if page === 9}
-		<div class="card w-2/3 p4 mt-5 bg-base-100 shadow-xl">
-			<div class="card-body">
-				<h2 class="card-title">Reflect</h2>
-				<p>
-					Take a minute to reflect on what you learned in this run. Maybe there is something you
-					want to note down?
 				</p>
 				<div class="card-actions justify-end">
 					<button
